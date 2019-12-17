a816a12b1f3efdd3131941f25c2b8f169ceb3cc9
zhaozhenghao <zhaozhenghao@nextjoy.com>
2019-12-17 18:42:46 +0800

    git 补充git build-dev打包

--- config/index.js
+++ config/index.js
@@ -45,12 +45,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, "../dist/index.html"),
+    index: path.resolve(__dirname, '../../dest/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, "../dist"),
+    assetsRoot: path.resolve(__dirname, '../../dest'),
     assetsSubDirectory: "static",
-    assetsPublicPath: "/",
+    assetsPublicPath: "//h5.baidu.com",
 
     /**
      * Source Maps
@@ -66,6 +66,36 @@ module.exports = {
     // npm install --save-dev compression-webpack-plugin
     productionGzip: false,
     productionGzipExtensions: ["js", "css"],
+
+    // Run the build command with an extra argument to
+    // View the bundle analyzer report after build finishes:
+    // `npm run build --report`
+    // Set to `true` or `false` to always turn it on or off
+    bundleAnalyzerReport: process.env.npm_config_report
+  },
+  dev_build: {
+    // Template for index.html
+    index: path.resolve(__dirname, '../../dest/index.html'),
+
+    // Paths
+    assetsRoot: path.resolve(__dirname, '../../dest'),
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '//h5-test.baidu.com/', //测试服打包放开
+
+    /**
+     * Source Maps
+     */
+
+    productionSourceMap: false,
+    // https://webpack.js.org/configuration/devtool/#production
+    devtool: '#source-map',
+
+    // Gzip off by default as many popular static hosts such as
+    // Surge or Netlify already gzip all static assets for you.
+    // Before setting to `true`, make sure to:
+    // npm install --save-dev compression-webpack-plugin
+    productionGzip: false,
+    productionGzipExtensions: ['js', 'css'],
 
     // Run the build command with an extra argument to
     // View the bundle analyzer report after build finishes:
