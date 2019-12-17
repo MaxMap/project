334831a6c12e84e317f4b41b653134bb0451de16
zhaozhenghao <zhaozhenghao@nextjoy.com>
2019-12-17 18:05:03 +0800

    还原

--- config/index.js
+++ config/index.js
@@ -1,14 +1,24 @@
 "use strict";
 // Template version: 1.3.1
 // see http://vuejs-templates.github.io/webpack for documentation.
-
+const linkurl = require('./common.config')
 const path = require("path");
+
 module.exports = {
   dev: {
-    // Paths
+    // Paths https://enigmatic-island-47099.herokuapp.com
     assetsSubDirectory: "static",
     assetsPublicPath: "/",
-    proxyTable: {},
+    proxyTable: {
+      "/api": {
+        target: linkurl.baseUrl.api,
+        changeOrigin: true,
+        secure: false,
+        pathRewrite: {
+          "^/api": ""
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: "localhost", // can be overwritten by process.env.HOST
@@ -35,48 +45,18 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, "../../gameCenter/index.html"),
+    index: path.resolve(__dirname, "../dist/index.html"),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, "../../gameCenter"),
+    assetsRoot: path.resolve(__dirname, "../dist"),
     assetsSubDirectory: "static",
-    assetsPublicPath: "//mgapi.nextjoy.com/gameCenter/", //线上打包放开
+    assetsPublicPath: "/",
 
     /**
      * Source Maps
      */
 
     productionSourceMap: true,
-    // https://webpack.js.org/configuration/devtool/#production
-    devtool: "#source-map",
-
-    // Gzip off by default as many popular static hosts such as
-    // Surge or Netlify already gzip all static assets for you.
-    // Before setting to `true`, make sure to:
-    // npm install --save-dev compression-webpack-plugin
-    productionGzip: false,
-    productionGzipExtensions: ["js", "css"],
-
-    // Run the build command with an extra argument to
-    // View the bundle analyzer report after build finishes:
-    // `npm run build --report`
-    // Set to `true` or `false` to always turn it on or off
-    bundleAnalyzerReport: process.env.npm_config_report
-  },
-  dev_build: {
-    // Template for index.html
-    index: path.resolve(__dirname, "../../gameCenter/index.html"),
-
-    // Paths
-    assetsRoot: path.resolve(__dirname, "../../gameCenter"),
-    assetsSubDirectory: "static",
-    assetsPublicPath: "//mtest.game.nextjoy.com/gameCenter/", //测试服打包放开
-
-    /**
-     * Source Maps
-     */
-
-    productionSourceMap: false,
     // https://webpack.js.org/configuration/devtool/#production
     devtool: "#source-map",
 
